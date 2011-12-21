b678602930ff0110981d2a3ad2582785aeb05c8b
Vincent Wang <vincent@linsong.org>
2011-12-21 21:58:25 +0800

    use tarball from office ftp site

--- duplicates/openssh.rb
+++ duplicates/openssh.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Openssh < Formula
-  url 'ftp://ftp.lambdaserver.com/pub/OpenBSD/OpenSSH/portable/openssh-5.9p1.tar.gz'
+  url 'ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/openssh-5.9.tar.gz'
   homepage 'http://www.openssh.com/'
   sha1 'bc0cb728bbc394769f9a2ce5b8cd99dc41e12632'
-  version '5.9p1'
+  version '5.9'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--with-libedit", "--prefix=#{prefix}"
