22488ee211b400d95fb29be217d6fcd0ae87ad0a
Vincent Wang <linsong.qizi@gmail.com>
2011-12-21 21:51:30 +0800

    Update duplicates/openssh.rb

--- duplicates/openssh.rb
+++ duplicates/openssh.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Openssh < Formula
-  url 'ftp://ftp.lambdaserver.com/pub/OpenBSD/OpenSSH/portable/openssh-5.9p1.tar.gz'
+  url 'ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/openssh-5.9.tar.gz'
   homepage 'http://www.openssh.com/'
   sha1 'ac4e0055421e9543f0af5da607a72cf5922dcc56'
   version '5.9p1'
